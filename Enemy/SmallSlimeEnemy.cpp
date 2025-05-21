e1e60333ba68612857bc2e38e78002baab1fd97d
NoodlesK <65808923+Noodle4219005@users.noreply.github.com>
2025-05-21 21:01:49 +0800

    Fix: SmallSlimeEnemy Speed Too Fast

--- Enemy/SmallSlimeEnemy.cpp
+++ Enemy/SmallSlimeEnemy.cpp
@@ -1,7 +1,7 @@
 #include "SmallSlimeEnemy.h"
 
 SmallSlimeEnemy::SmallSlimeEnemy(int x, int y) : 
-  Enemy("play/enemy-1.png", x, y, 1, 727, 1, 5) 
+  Enemy("play/enemy-1.png", x, y, 1, 727/2, 1, 5) 
 {
     Enemy::Tint=al_map_rgb(255, 0, 0);
 }
